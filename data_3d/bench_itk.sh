17ffaa60124aa4c08e0ce3d69504b6b99de25474
lebonq <lebonquentin@live.fr>
2024-03-01 14:29:54 +0100

    Add itk SH file

---
+++ data_3d/bench_itk.sh
@@ -0,0 +1,7 @@
+
+/home/lebonq/.local/share/virtualenvs/data_3d-DCEnfaQi/bin/python /home/lebonq/CLionProjects/incremental_watershed/data_3d/itk_expriment.py patient_2  
+/home/lebonq/.local/share/virtualenvs/data_3d-DCEnfaQi/bin/python /home/lebonq/CLionProjects/incremental_watershed/data_3d/itk_expriment.py patient_7  
+/home/lebonq/.local/share/virtualenvs/data_3d-DCEnfaQi/bin/python /home/lebonq/CLionProjects/incremental_watershed/data_3d/itk_expriment.py patient_12  
+/home/lebonq/.local/share/virtualenvs/data_3d-DCEnfaQi/bin/python /home/lebonq/CLionProjects/incremental_watershed/data_3d/itk_expriment.py patient_19  
+/home/lebonq/.local/share/virtualenvs/data_3d-DCEnfaQi/bin/python /home/lebonq/CLionProjects/incremental_watershed/data_3d/itk_expriment.py patient_20  
+
