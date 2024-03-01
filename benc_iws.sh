12c74b679da44dde8a904577276beebcb6e03b7c
lebonq <lebonquentin@live.fr>
2024-03-01 17:13:35 +0100

    Upadte bench sh

--- benc_iws.sh
+++ benc_iws.sh
@@ -1,3 +1,7 @@
+/home/lebonq/.local/share/JetBrains/Toolbox/apps/clion-nova/bin/cmake/linux/x64/bin/cmake --build /home/lebonq/CLionProjects/incremental_watershed/cmake-build-release --target incremental_watershed_framework-3d-iws -j 18
+
+# Build line is to update accoridng to your dev environment
+
 /home/lebonq/CLionProjects/incremental_watershed/cmake-build-release/src/framework/incremental_watershed_framework-3d data_3d/patient_2/patient_2_png data_3d/patient_2/patient_2_liver_markers 30 10;
 /home/lebonq/CLionProjects/incremental_watershed/cmake-build-release/src/framework/incremental_watershed_framework-3d data_3d/patient_7/patient_7_png data_3d/patient_7/patient_7_liver_markers 30 10;
 /home/lebonq/CLionProjects/incremental_watershed/cmake-build-release/src/framework/incremental_watershed_framework-3d data_3d/patient_12/patient_12_png data_3d/patient_12/patient_12_liver_markers 30 10;
